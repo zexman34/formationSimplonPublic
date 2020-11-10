84dc5a14926e34ab440f4a464820dcd5bf0eef8f
FabienB <73230239+zexman34@users.noreply.github.com>
2020-11-10 09:51:00 +0100

    Add files via upload

---
+++ Exo14/page-telecommande.js
@@ -0,0 +1,24 @@
+"user strict";
+
+// j'écoute l'évènement "click" de la première touche de ma télécommande
+document.querySelector("main > ul > li:nth-of-type(1)").addEventListener("click", (event) => {
+  let bruit = new Audio("https://hmz.tf/static/audio-chat.mp3"); // je créé un objet audio appelé "bruit"
+  bruit.play(); // je joue ce bruit
+});
+
+// même chose avec les autres touches de ma télécommande
+
+document.querySelector("main > ul > li:nth-of-type(2)").addEventListener("click", (event) => {
+  let bruit = new Audio("https://hmz.tf/static/audio-sncf.mp3");
+  bruit.play();
+});
+
+document.querySelector("main > ul > li:nth-of-type(3)").addEventListener("click", (event) => {
+  let bruit = new Audio("https://hmz.tf/static/audio-burp.mp3");
+  bruit.play();
+});
+
+document.querySelector("main > ul > li:nth-of-type(4)").addEventListener("click", (event) => {
+  let bruit = new Audio("https://github.com/zexman34/formationSimplonPublic/blob/main/Exo14/punch-gaming-sound-effect-hd.mp3");
+  bruit.play();
+});
