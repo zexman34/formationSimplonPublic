ec8d7c94a12fe0be867886b696a1c0e37c46c9b7
FabienB <73230239+zexman34@users.noreply.github.com>
2020-11-10 10:53:33 +0100

    Add files via upload

--- Exo14/page-telecommande.js
+++ Exo14/page-telecommande.js
@@ -1,24 +1,34 @@
 "user strict";
 
 // j'écoute l'évènement "click" de la première touche de ma télécommande
+//Voiture
 document.querySelector("main > ul > li:nth-of-type(1)").addEventListener("click", (event) => {
-  let bruit = new Audio("https://hmz.tf/static/audio-chat.mp3"); // je créé un objet audio appelé "bruit"
+  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Sons/voiture.mp3"); // je créé un objet audio appelé "bruit"
   bruit.play(); // je joue ce bruit
 });
-
 // même chose avec les autres touches de ma télécommande
-
+//Cloche
 document.querySelector("main > ul > li:nth-of-type(2)").addEventListener("click", (event) => {
-  let bruit = new Audio("https://hmz.tf/static/audio-sncf.mp3");
+  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Sons/cloche.mp3");
   bruit.play();
 });
-
+//Coq
 document.querySelector("main > ul > li:nth-of-type(3)").addEventListener("click", (event) => {
-  let bruit = new Audio("https://hmz.tf/static/audio-burp.mp3");
+  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Sons/coq.mp3");
   bruit.play();
 });
-
+//Violon
 document.querySelector("main > ul > li:nth-of-type(4)").addEventListener("click", (event) => {
-  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Punch_Swoosh_Series.mp3");
+  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Sons/violon.mp3");
+  bruit.play();
+});
+//Klaxon
+document.querySelector("main > ul > li:nth-of-type(5)").addEventListener("click", (event) => {
+  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Sons/klaxon.mp3");
+  bruit.play();
+});
+//Travaux
+document.querySelector("main > ul > li:nth-of-type(6)").addEventListener("click", (event) => {
+  let bruit = new Audio("https://raw.githubusercontent.com/zexman34/formationSimplonPublic/main/Exo14/Sons/travaux.mp3");
   bruit.play();
 });
